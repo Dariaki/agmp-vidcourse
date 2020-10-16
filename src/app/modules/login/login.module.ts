5ada96c55af26bedcf4b6bf13118f9b6892859f5
Daria_Kyrychenko <dk>
2020-10-16 21:07:04 +0300

    Task 1: removed core module, made one header, re-structured components

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
-import { LoginHeaderComponent } from './components/login-header/login-header.component';
 
 @NgModule({
     declarations: [
       LoginComponent,
-      LoginFormComponent,
-      LoginHeaderComponent
+      LoginFormComponent
     ],
   imports: [
     SharedModule,
